435c167ac6d30baf5a521c2853bdb3ba70352966
Adrian Stainforth <adrian@morsedigital.com>
2016-01-26 13:12:04 +0000

    dependancy tweak

--- gulp_tasks/karma.js
+++ gulp_tasks/karma.js
@@ -1,83 +1,83 @@
-var gulp         = require('gulp');
-var gutil        = require('gulp-util');
-var karma        = require('gulp-karma');
+// var gulp         = require('gulp');
+// var gutil        = require('gulp-util');
+// var karma        = require('gulp-karma');
 
-var testFiles = [
-  'spec/manage_cookies_spec.coffee'
-];
+// var testFiles = [
+//   'spec/manage_cookies_spec.coffee'
+// ];
 
-var karma_config = "./gulp_tasks/karma.conf.js"
+// var karma_config = "./gulp_tasks/karma.conf.js"
 
-// <<<<<<<<<<<<<< Karma tasks >>>>>>>>>>>>>
+// // <<<<<<<<<<<<<< Karma tasks >>>>>>>>>>>>>
 
-gulp.task('karma', function() {
-  gulp.src(testFiles)
-    .pipe(karma({
-      configFile: karma_config,
-      action: 'watch',
-      browsers: ['PhantomJS']
-    }));
-});
+// gulp.task('karma', function() {
+//   gulp.src(testFiles)
+//     .pipe(karma({
+//       configFile: karma_config,
+//       action: 'watch',
+//       browsers: ['PhantomJS']
+//     }));
+// });
 
-gulp.task('karma:semaphore', function() {
-  gulp.src(testFiles)
-    .pipe(karma({
-      configFile: karma_config,
-      action: 'run',
-      browsers: ['PhantomJS']
-    }));
-});
-
-gulp.task('karma:all', function() {
-  gulp.src(testFiles)
-    .pipe(karma({
-      configFile: karma_config,
-      action: 'run'
-    }));
-});
-
-gulp.task('karma:mac', function() {
-  gulp.src(testFiles)
-    .pipe(karma({
-      configFile: karma_config,
-      action: 'run',
-      browsers: ['Chrome', 'Firefox', 'Safari', 'Opera'],
-    }));
-});
-
-// Not currently workingon Mavericks os
-// gulp.task('karma:iOS', function() {
+// gulp.task('karma:semaphore', function() {
 //   gulp.src(testFiles)
 //     .pipe(karma({
 //       configFile: karma_config,
 //       action: 'run',
-//       browsers: ['iOS'],
+//       browsers: ['PhantomJS']
 //     }));
 // });
 
-gulp.task('karma:ie9', function() {
-  gulp.src(testFiles)
-    .pipe(karma({
-      configFile: karma_config,
-      action: 'run',
-      browsers:['IE9 - Win7'],
-    }));
-});
+// gulp.task('karma:all', function() {
+//   gulp.src(testFiles)
+//     .pipe(karma({
+//       configFile: karma_config,
+//       action: 'run'
+//     }));
+// });
 
-gulp.task('karma:ie10', function() {
-  gulp.src(testFiles)
-    .pipe(karma({
-      configFile: karma_config,
-      action: 'run',
-      browsers:['IE10 - Win7'],
-    }));
-});
+// gulp.task('karma:mac', function() {
+//   gulp.src(testFiles)
+//     .pipe(karma({
+//       configFile: karma_config,
+//       action: 'run',
+//       browsers: ['Chrome', 'Firefox', 'Safari', 'Opera'],
+//     }));
+// });
 
-gulp.task('karma:ie11', function() {
-  gulp.src(testFiles)
-    .pipe(karma({
-      configFile: karma_config,
-      action: 'run',
-      browsers:['IE11 - Win7'],
-    }));
-});
+// // Not currently workingon Mavericks os
+// // gulp.task('karma:iOS', function() {
+// //   gulp.src(testFiles)
+// //     .pipe(karma({
+// //       configFile: karma_config,
+// //       action: 'run',
+// //       browsers: ['iOS'],
+// //     }));
+// // });
+
+// gulp.task('karma:ie9', function() {
+//   gulp.src(testFiles)
+//     .pipe(karma({
+//       configFile: karma_config,
+//       action: 'run',
+//       browsers:['IE9 - Win7'],
+//     }));
+// });
+
+// gulp.task('karma:ie10', function() {
+//   gulp.src(testFiles)
+//     .pipe(karma({
+//       configFile: karma_config,
+//       action: 'run',
+//       browsers:['IE10 - Win7'],
+//     }));
+// });
+
+// gulp.task('karma:ie11', function() {
+//   gulp.src(testFiles)
+//     .pipe(karma({
+//       configFile: karma_config,
+//       action: 'run',
+//       browsers:['IE11 - Win7'],
+//     }));
+// });
--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-        '../spec/*.es6.js'
+        './spec/*.es6.js'
     ],
 
 
@@ -27,7 +27,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-        '../spec/*.es6.js': [ 'browserify' ]
+        './spec/*.es6.js': [ 'browserify' ]
     },
 
     browserify: {
